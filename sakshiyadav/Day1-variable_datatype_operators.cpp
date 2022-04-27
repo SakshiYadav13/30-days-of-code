a1e1c66c76ed0a2b4e1a52e1d97d68364d1c9e14
SakshiYadav13 <88963135+SakshiYadav13@users.noreply.github.com>
2022-04-27 14:01:19 +0530

    Create Day1-variable_datatype_operators.cpp

---
+++ sakshiyadav/Day1-variable_datatype_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int a, b, sum;
+    cout<<"enter first no. \n"<<endl;
+    cin>>a;
+    cout<<"enter second no. \n"<<endl;
+    cin>>b;
+    sum=a+b;
+    cout<<"sum of two no.s is \n"<<sum;
+    cin>>sum;
+    return 0;
+}
