111436e931de883b3b76203228675b530c7cba29
SakshiYadav13 <88963135+SakshiYadav13@users.noreply.github.com>
2022-05-07 22:14:32 +0530

    Create Day14-LAZYCHF.cpp

---
+++ sakshiyadav/Day14-LAZYCHF.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,m,d;
+	    cin>>x>>m>>d;
+	    if((x*m)>=(x+d)){
+	        cout<<x+d<<endl;
+	    }
+	    else{
+	        cout<<x*m<<endl;
+	    }
+	}
+}
