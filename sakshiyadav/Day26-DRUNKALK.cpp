ee55f65a404f45887898f44157c8ae0b60bc933e
SakshiYadav13 <88963135+SakshiYadav13@users.noreply.github.com>
2022-05-21 10:28:53 +0530

    Create Day26-DRUNKALK.cpp

---
+++ sakshiyadav/Day26-DRUNKALK.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int k;
+	    cin>>k;
+	    int m;
+	    m=k+2;
+	    if(k%2==0){
+	        cout<<k<<endl;
+	    }
+	    else{
+	        cout<<m<<endl;
+	    }
+	}
+	return 0;
+}
